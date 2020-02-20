3c2798dcc42ad99a8ad2a0a3797f6b68c4c5cde8
Dang Mai <contact@dangmai.net>
2020-02-20 13:44:38 -0500

    Initial attempt to test the package using Arch docker container

---
+++ scripts/sfdx-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+cd package
+makepkg --noconfirm -si
+SRC_VERSION=$(grep "pkgver" .SRCINFO | cut -f2 -d '=' | xargs)
+INSTALLED_VERSION=$(sfdx --version | cut -f1 -d' ' | cut -f 2 -d '/')
+
+if [ "$SRC_VERSION" != "${INSTALLED_VERSION/-/_}" ]; then
+  exit 1
+fi

06fa8bf1f774610c4eeba3daa4968f06fae2713d
Dang Mai <contact@dangmai.net>
2020-02-21 16:03:23 -0500

    Use builduser home dir to generate SRCINFO

--- .github/actions/generate-srcinfo/srcinfo-entrypoint.sh
+++ .github/actions/generate-srcinfo/srcinfo-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+cd /home/builduser
 SRCINFO=$(makepkg --printsrcinfo)
 VERSION=$(echo "${SRCINFO}" | grep "pkgver" | cut -f2 -d '=' | xargs)
 echo ::set-output name=srcinfo::"${SRCINFO}"

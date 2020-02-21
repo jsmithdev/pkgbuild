d85d9ab7390e35edcdb7430a1c3c9639c759246c
Dang Mai <contact@dangmai.net>
2020-02-21 16:07:51 -0500

    Escape multiline string

--- .github/actions/generate-srcinfo/srcinfo-entrypoint.sh
+++ .github/actions/generate-srcinfo/srcinfo-entrypoint.sh
@@ -4,5 +4,10 @@ set -euxo pipefail
 cd /home/builduser
 SRCINFO=$(makepkg --printsrcinfo)
 VERSION=$(echo "${SRCINFO}" | grep "pkgver" | cut -f2 -d '=' | xargs)
+# Escape multiline strings, see:
+# https://github.community/t5/GitHub-Actions/set-output-Truncates-Multiline-Strings/m-p/37870
+SRCINFO="${SRCINFO//'%'/'%25'}"
+SRCINFO="${SRCINFO//$'\n'/'%0A'}"
+SRCINFO="${SRCINFO//$'\r'/'%0D'}"
 echo ::set-output name=srcinfo::"${SRCINFO}"
 echo ::set-output name=version::"${VERSION}"

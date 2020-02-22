00f24acda3f7645ac7973dcb6fe7d62cb1a0883a
Dang Mai <contact@dangmai.net>
2020-02-21 22:42:59 -0500

    Use sh instead of bash inside alpine container

--- .github/actions/aur/aur-entrypoint.sh
+++ .github/actions/aur/aur-entrypoint.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
-set -euxo pipefail
+#!/bin/sh
+set -eux
 
 git clone ssh://aur@aur.archlinux.org/sfdx-cli.git /git/sfdx-cli
 cd /git/sfdx-cli
 touch test.txt
-if [[ -n $(git status -s) ]]; then
+if [ -n "$(git status -s)" ]; then
   git add .
   git config user.name "Dang Mai"
   git config user.email "contact@dangmai.net"

1ceb637a9a61597f3aa456f537d8e7c4c4c14a84
Dang Mai <contact@dangmai.net>
2021-06-12 14:30:26 -0400

    Release new version

--- .github/actions/sfdx-test/sfdx-entrypoint.sh
+++ .github/actions/sfdx-test/sfdx-entrypoint.sh
@@ -6,6 +6,6 @@ makepkg --noconfirm -si
 SRC_VERSION=$(grep "pkgver" .SRCINFO | cut -f2 -d '=' | xargs)
 INSTALLED_VERSION=$(sfdx --version | cut -f1 -d' ' | cut -f 2 -d '/')
 
-if [ "$SRC_VERSION" != "${INSTALLED_VERSION/-/_}" ]; then
+if [ "$SRC_VERSION" != "$INSTALLED_VERSION" ]; then
   exit 1
 fi
